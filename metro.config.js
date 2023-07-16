6615c7f357963d7f61f5aa1bddd93fd45c60bae4
thansih-ahammed <thansihahammed7356@gmail.com>
2023-07-16 07:44:33 +0530

    start project

---
+++ metro.config.js
@@ -0,0 +1,26 @@
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+
+  const updatedSourceExits = [...sourceExts, "jsx", "ts" ,"tsx"]
+
+  return {
+    transformer: {
+        getTransformOptions: () =>({
+              transform:{
+                 experimentalImportSupport:false,
+                 inlineRequires:true,
+              },
+        }),
+
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...updatedSourceExits, "svg"]
+    }
+  };
+})();
